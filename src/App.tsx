5619d646932543a8974306956e569a30324fa824
andrewdahn <andrewahnjr@gmail.com>
2023-02-08 20:56:28 -0800

    transaction table

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
   };
 
   return (
-    <div>
+    <div className='container mx-auto'>
       <TransactionsTable transactions={txs} />
     </div>
   );
--- src/components/TransactionsTable/TransactionsTable.tsx
+++ src/components/TransactionsTable/TransactionsTable.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import Transaction from '../../types/tx';
-import {
-  convertTime,
-  formatAddr,
-  addressLink,
-  txLink,
-  shortenTxId,
-} from '../../utils';
+import { convertTime, formatAddr, addressLink, txLink } from '../../utils';
+
+const HEADERS = ['Transaction', 'Amount', 'Swap', 'Time'];
 
 interface Props {
   transactions: Transaction[];
@@ -16,69 +12,68 @@ const TransactionsTable: React.FC<Props> = ({ transactions }) => {
   console.log(transactions);
 
   return (
-    <div>
-      <table className='table-auto'>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Total Amount</th>
-            <th>Swaps</th>
-            <th>Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          {transactions.map((transaction) => {
-            return (
-              <tr>
-                <td>
-                  <a
-                    href={txLink(transaction.id)}
-                    target='_blank'
-                    rel='noreferrer'
-                  >
-                    {shortenTxId(transaction.id)}
-                  </a>
-                </td>
-                <td></td>
-                <td>
-                  {transaction.swaps.map((swap) => {
-                    return (
-                      <div className='flex flex-col'>
-                        {/* Sender */}
-                        <span>
-                          Sender:{' '}
-                          <a
-                            href={addressLink(swap.recipient)}
-                            target='_blank'
-                            rel='noreferrer'
-                            className='text-blue-500'
-                          >
-                            {formatAddr(swap.sender)}
-                          </a>
-                        </span>
-                        {/* Recipient */}
-                        <span>
-                          Recipient:{' '}
-                          <a
-                            href={addressLink(swap.recipient)}
-                            target='_blank'
-                            rel='noreferrer'
-                            className='text-red-500'
-                          >
-                            {formatAddr(swap.recipient)}
-                          </a>
-                        </span>
-                      </div>
-                    );
-                  })}
-                </td>
-                <td>{convertTime(transaction.timestamp)}</td>
-              </tr>
-            );
+    <table className='min-w-full'>
+      <thead>
+        <tr className='bg-gray-100'>
+          {HEADERS.map((header) => {
+            return <th className='border px-4 py-6'>{header}</th>;
           })}
-        </tbody>
-      </table>
-    </div>
+        </tr>
+      </thead>
+      <tbody>
+        {transactions.map((transaction) => {
+          return (
+            <tr className='even:bg-gray-100 odd:bg-white'>
+              <td className='border p-3 text-blue-400'>
+                <a
+                  href={txLink(transaction.id)}
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  {transaction.id}
+                </a>
+              </td>
+              <td className='border px-4 py-2'>Fill in later</td>
+              <td className='border px-4 py-2 divide-y'>
+                {transaction.swaps.map((swap) => {
+                  return (
+                    <div className='flex flex-col py-1'>
+                      {/* Sender */}
+                      <span>
+                        Sender:{' '}
+                        <a
+                          href={addressLink(swap.recipient)}
+                          target='_blank'
+                          rel='noreferrer'
+                          className='text-blue-400'
+                        >
+                          {formatAddr(swap.sender)}
+                        </a>
+                      </span>
+                      {/* Recipient */}
+                      <span>
+                        Recipient:{' '}
+                        <a
+                          href={addressLink(swap.recipient)}
+                          target='_blank'
+                          rel='noreferrer'
+                          className='text-blue-400'
+                        >
+                          {formatAddr(swap.recipient)}
+                        </a>
+                      </span>
+                    </div>
+                  );
+                })}
+              </td>
+              <td className='border px-4 py-2'>
+                {convertTime(transaction.timestamp)}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
